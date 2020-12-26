28b57304a3d4ef2a4951faa2b38bb3f47af054c1
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-12-26 13:53:12 -0600

    Redo the logic behind find_at_path

--- src/lib/finder.rs
+++ src/lib/finder.rs
@@ -2,9 +2,10 @@ extern crate dirs;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
-
 pub fn find_at_path(path: &str, mime_type: &str) -> Option<Vec<String>> {
-    let mut our_matches: Vec<String> = Vec::new();
+    let mut line_match = String::new();
+
+    let mut match_vector: Vec<String> = Vec::new();
 
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
@@ -12,14 +13,20 @@ pub fn find_at_path(path: &str, mime_type: &str) -> Option<Vec<String>> {
     for line in reader.lines() {
         let raw_line = line.unwrap();
         if raw_line.contains(mime_type) {
-            our_matches.push(raw_line)
+            line_match = raw_line
         }
     }
 
-    if our_matches.len() > 0 {
-        Some(our_matches)
+    if !line_match.is_empty() {
+        let replace_pattern = format!("{}=", mime_type);
+        line_match = line_match.replace(&replace_pattern, "");
+        for entry in line_match.trim().split(";") {
+            if !entry.is_empty() {
+                match_vector.push(entry.to_string());  
+            }
+        }
+        Some(match_vector)
     } else {
-        None 
+        None
     }
-
 }
